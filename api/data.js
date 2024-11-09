38960e3636741c3d758aa2a2ece64c634d4d9dcc
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-09 18:43:32 +0100

    modified:   api/data.js

--- api/data.js
+++ api/data.js
@@ -1,6 +1,7 @@
 // /api/data.js
 const mysql = require('mysql2');
 
+// Configura la conexión solo una vez
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -8,20 +9,28 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
+// Verifica la conexión
+connection.connect((err) => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err);
+  } else {
+    console.log('Conexión a la base de datos establecida');
+  }
+});
+
 module.exports = (req, res) => {
-    
+  // Confirma si el método es GET
   if (req.method === 'GET') {
-     
-    // Si la solicitud es GET, obtiene los datos
+    // Realiza la consulta a la base de datos
     connection.query('SELECT * FROM clientes', (error, results) => {
       if (error) {
-        console.error('Error en la consulta: ', error);
+        console.error('Error en la consulta:', error);
         return res.status(500).json({ error: 'Error en la consulta' });
       }
-      res.json(results);
+      res.status(200).json(results);
     });
   } else {
-    // Si no es un GET, responde con un 405 (Método no permitido)
+    // Responde con un 405 si el método no es GET
     res.status(405).send('Método no permitido');
   }
 };

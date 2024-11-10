c379c48f5ce185fb2f9fd736f32ad555a1756a26
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-10 15:27:19 +0100

    modified:   db.js 	modified:   db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 // db.js
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
 const getConnection = async () => {
-  return await mysql.createConnection({
+  return mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,

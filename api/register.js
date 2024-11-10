f06bafd923a3fa4578d2fbb4ebe752353cfe8bd9
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-10 17:34:53 +0100

    modified:   api/register.js

--- api/register.js
+++ api/register.js
@@ -5,9 +5,8 @@ module.exports = (req, res) => {
   // if (req.method === 'POST') {
   const { nombre, apellido1, apellido2, email, contraseña, telefono } =
     req.body;
-  const query =
-    "INSERT INTO clientes (nombre, apellido1, apellido2, email, contraseña, telefono, fecha_registro) VALUES (?, ?, ?, ?, ?, ?, NOW())";
-  // //   const query = 'INSERT INTO clientes (nombre, apellido1, apellido2, email, contraseña, telefono, fecha_registro) VALUES ("juan", "apellido1", "apellido2", "juan@gmail.com", "123456789", "123456789", NOW())';
+  //const query = "INSERT INTO clientes (nombre, apellido1, apellido2, email, contraseña, telefono, fecha_registro) VALUES (?, ?, ?, ?, ?, ?, NOW())";
+  const query = 'INSERT INTO clientes (nombre, apellido1, apellido2, email, contraseña, telefono, fecha_registro) VALUES ("juan", "apellido1", "apellido2", "juan@gmail.com", "123456789", "123456789", NOW())';
 
   connection().then((conn) =>
     conn.query(
@@ -26,7 +25,7 @@ module.exports = (req, res) => {
     )
   );
 
-  res.status(200).send("Método 200");
+ 
   // }
   // else {
   //   res.status(405).send('Método no permitido');

8e694d082cd285cd1be05a018c2f9dc900e767d3
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-10 16:03:41 +0100

    modified:   api/register.js 	modified:   db.js

--- api/register.js
+++ api/register.js
@@ -1,21 +1,21 @@
 // /api/register.js
-// const mysql = require('mysql2');
+const mysql = require('mysql2');
 const connection = require('../db'); // Importar la conexión
 
 module.exports = (req, res) => {
     // if (req.method === 'POST') {
       const { nombre, apellido1, apellido2, email, contraseña, telefono } = req.body;
     const query = 'INSERT INTO clientes (nombre, apellido1, apellido2, email, contraseña, telefono, fecha_registro) VALUES (?, ?, ?, ?, ?, ?, NOW())';
-    //const query = 'INSERT INTO clientes (nombre, apellido1, apellido2, email, contraseña, telefono, fecha_registro) VALUES ("juan", "apellido1", "apellido2", "juan@gmail.com", "123456789", "123456789", NOW())';
+    //   const query = 'INSERT INTO clientes (nombre, apellido1, apellido2, email, contraseña, telefono, fecha_registro) VALUES ("juan", "apellido1", "apellido2", "juan@gmail.com", "123456789", "123456789", NOW())';
 
       
-        connection().then(conn => conn.query( query, [nombre, apellido1, apellido2, email, contraseña, telefono], (error, results) => {
+        connection.query(query, [nombre, apellido1, apellido2, email, contraseña, telefono], (error, results) => {
             if (error) {
               console.error('Error en la consulta: ', error);  // Imprime el error en el log de Vercel
               return res.status(500).json({ error: 'Error al registrar el cliente', details: error.message });
             }
             res.status(201).send('Cliente registrado con éxito');
-        }));
+        });
       
       
      
--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 // db.js
 const mysql = require('mysql2/promise');
 
-const getConnection =  () => {
-  return  mysql.createConnection({
+const getConnection = async () => {
+  return await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,

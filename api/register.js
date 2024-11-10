cbdea0b4b963a68403cf174ae30f8cbea2a2de3f
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-10 14:29:38 +0100

    modified:   api/register.js

--- api/register.js
+++ api/register.js
@@ -6,14 +6,16 @@ module.exports = (req, res) => {
       const { nombre, apellido1, apellido2, email, contraseña, telefono } = req.body;
       const query = 'INSERT INTO clientes (nombre, apellido1, apellido2, email, contraseña, telefono, fecha_registro) VALUES (?, ?, ?, ?, ?, ?, NOW())';
   
+      
+      connection.query(query, [nombre, apellido1, apellido2, email, contraseña, telefono], (error, results) => {
+        if (error) {
+          console.error('Error en la consulta: ', error);  // Imprime el error en el log de Vercel
+          return res.status(500).json({ error: 'Error al registrar el cliente', details: error.message });
+        }
+        res.status(201).send('Cliente registrado con éxito');
+      });
+
       res.status(405).send(req.body);
-    //   connection.query(query, [nombre, apellido1, apellido2, email, contraseña, telefono], (error, results) => {
-    //     if (error) {
-    //       console.error('Error en la consulta: ', error);  // Imprime el error en el log de Vercel
-    //       return res.status(500).json({ error: 'Error al registrar el cliente', details: error.message });
-    //     }
-    //     res.status(201).send('Cliente registrado con éxito');
-    //   });
     // } 
     // else {
     //   res.status(405).send('Método no permitido');

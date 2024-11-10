77fe21acd1f8c3129bcf076f63f670c70cd963a7
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-10 15:49:16 +0100

    modified:   db.js

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ const mysql = require('mysql2/promise');
 const getConnection =  () => {
   return  mysql.createConnection({
     host: "velasshopping.c94kw4uiss1b.eu-west-2.rds.amazonaws.com",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    user: "velasShoping",
+    password: "velasShoping2024",
+    database: "velasshoping",
     port: 3306,
   });
 };

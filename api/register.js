df1d2518c587df7c848eaf876696a11050a9bcb8
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-10 16:34:05 +0100

    modified:   api/register.js 	modified:   db.js

--- api/register.js
+++ api/register.js
@@ -1,20 +1,20 @@
 // /api/register.js
 const connection = require('../db'); // Importar la conexión
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
     // if (req.method === 'POST') {
       const { nombre, apellido1, apellido2, email, contraseña, telefono } = req.body;
     const query = 'INSERT INTO clientes (nombre, apellido1, apellido2, email, contraseña, telefono, fecha_registro) VALUES (?, ?, ?, ?, ?, ?, NOW())';
     //   const query = 'INSERT INTO clientes (nombre, apellido1, apellido2, email, contraseña, telefono, fecha_registro) VALUES ("juan", "apellido1", "apellido2", "juan@gmail.com", "123456789", "123456789", NOW())';
 
       
-        await connection().execute(query, [nombre, apellido1, apellido2, email, contraseña, telefono], (error, results) => {
+        connection().then(conn=>conn.query(query, [nombre, apellido1, apellido2, email, contraseña, telefono], (error, results) => {
             if (error) {
               console.error('Error en la consulta: ', error);  // Imprime el error en el log de Vercel
               return res.status(500).json({ error: 'Error al registrar el cliente', details: error.message });
             }
             res.status(201).send('Cliente registrado con éxito');
-        });
+        }));
       
 
       // } 

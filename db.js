2c62c789fc7bf767c99ed0aea03f825302d1c1ad
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-10 16:39:07 +0100

    modified:   db.js 	modified:   test.html

--- db.js
+++ db.js
@@ -3,8 +3,7 @@ const mysql = require('mysql2/promise');
 
 const getConnection =  () => {
   return  mysql.createConnection({
-    // host: process.env.DB_HOST,
-    host: "hola",
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,

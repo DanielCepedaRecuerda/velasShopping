67fc740d83183869a97ce6c1aba8876924a228d6
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-17 13:51:10 +0100

    new file:   controllers/ProductController.js 	modified:   db/connection.js 	new file:   models/ProductModel.js 	renamed:    app.js -> public/app.js 	renamed:    images/iconoInstagram.jpg -> public/images/iconoInstagram.jpg 	renamed:    "images/iconoP\303\241gina.jpg" -> "public/images/iconoP\303\241gina.jpg" 	renamed:    images/vela1.jpg -> public/images/vela1.jpg 	renamed:    images/vela2.jpg -> public/images/vela2.jpg 	renamed:    images/velaDeCafe.jpg -> public/images/velaDeCafe.jpg 	renamed:    images/velaDeCeraDeAbeja.jpg -> public/images/velaDeCeraDeAbeja.jpg 	renamed:    images/velaEnAgua.jpg -> public/images/velaEnAgua.jpg 	renamed:    images/velaFormaRosas.jpg -> public/images/velaFormaRosas.jpg 	renamed:    images/velaNavidades.jpg -> public/images/velaNavidades.jpg 	renamed:    images/velaSanValentin.jpg -> public/images/velaSanValentin.jpg 	renamed:    images/velasAromaticas.jpg -> public/images/velasAromaticas.jpg 	renamed:    images/velasDecorativas.jpg -> public/images/velasDecorativas.jpg 	renamed:    images/velasHalloween.jpg -> public/images/velasHalloween.jpg 	renamed:    styles.css -> public/styles.css 	renamed:    login.html -> views/login.html

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise');
 
 const connection = async () => {
   return mysql.createPool({
-    host: 'localhost', // Cambiar según tu configuración
-    user: 'root',
-    password: 'password',
-    database: 'nombre_de_tu_base_de_datos',
+    host: process.env.DB_HOST, // Cambiar según tu configuración
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
   });

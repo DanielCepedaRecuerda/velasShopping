6ff68ec581fabfa15ef166ee7b74c9a07a7f4f28
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-17 15:19:12 +0100

    modified:   app.js 	deleted:    register.html 	modified:   views/contacto.html 	modified:   views/index.html 	modified:   views/login.html 	modified:   views/productos.html 	modified:   views/register.html 	modified:   views/velasAromaticas.html 	modified:   views/velasDecorativas.html 	modified:   views/velasTematicas.html

--- app.js
+++ app.js
@@ -14,10 +14,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Configurar las rutas
 app.use('/api/users', userRoutes);
 
+// Rutas para los archivos HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/productos', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'productos.html'));
+});
+
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/contacto', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contacto.html'));
+});
+
+app.get('/velasAromaticas', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'velasAromaticas.html'));
+});
+
+app.get('/velasTematicas', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'velasTematicas.html'));
+});
+
+app.get('/velasDecorativas', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'velasDecorativas.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
+
 // Servir vistas (opcional)
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 
-// Iniciar el servidor
+// Iniciar servidor
 app.listen(3000, () => {
-  console.log('Servidor corriendo en el puerto 3000');
+  console.log('Servidor corriendo en http://localhost:3000');
 });

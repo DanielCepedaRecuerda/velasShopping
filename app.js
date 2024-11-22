184aa00e9382df0c0677d70b5195d6c3f9b85015
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-22 12:45:15 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -76,6 +76,6 @@ app.get("/", (req, res) =>
 );
 
 // Iniciar servidor
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
-});
+// app.listen(3000, () => {
+//   console.log("Servidor corriendo en http://localhost:3000");
+// });

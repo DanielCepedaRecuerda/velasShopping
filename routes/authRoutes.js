46c342eebc3ed82f1bcf92f3e60665ad2d1f0df8
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-24 21:08:03 +0100

    modified:   routes/authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const userController = require('../controllers/UserController');
-
 const router = express.Router();
 
 // Ruta para registrar usuarios
 router.post('/register', userController.registerUser);
 // Ruta para login
 router.post("/api/login", userController.loginUser);
+// Ruta para logout
+router.get('/logout', userController.logoutUser);
 
 module.exports = router;

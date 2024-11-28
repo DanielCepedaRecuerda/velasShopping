a7b9ff85e053f34260625177073395dfa52f3334
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-11-28 11:46:38 +0100

    modified:   public/js/script.js 	modified:   views/productos.html

--- public/js/script.js
+++ public/js/script.js
@@ -30,7 +30,7 @@ if (cartCookie) {
   cartItems.forEach(item => {
     totalQuantity += item.quantity;  // Sumar las cantidades
   });
-
+  document.getElementById("item-count").textContent = totalQuantity;
   console.log("Total de productos en el carrito:", totalQuantity);  // Mostrar total
 } else {
   console.log("No hay carrito guardado en las cookies.");

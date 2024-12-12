e2f0f28a1fcfbd913fbc785b5915179f743e30b7
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-12-12 14:22:51 +0100

    modified:   db/connection.js 	modified:   routes/velasRoutes.js

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,20 @@
 const mysql = require("mysql2/promise");
 
 const connection = async () => {
-  return mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-  });
+  try {
+    return mysql.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: 3306,
+      waitForConnections: true,
+      connectionLimit: 10,
+    });
+  } catch (error) {
+    console.error("Error al crear el pool de conexiones:", error);
+    throw error;
+  }
 };
 
 module.exports = connection;
--- routes/velasRoutes.js
+++ routes/velasRoutes.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const velasController = require('../controllers/velasController');
 
 // Rutas para cada categoría
-router.get('/velasAromaticas', velasController.getVelasByCategoria);
-router.get('/velasTematicas', velasController.getVelasByCategoria);
-router.get('/velasDecorativas', velasController.getVelasByCategoria);
-
+router.get('/velasAromaticas', (req, res) => {
+    req.params.categoria = 'Aromaticas';
+    velasController.getVelasByCategoria(req, res);
+  });
+  
+  router.get('/velasTematicas', (req, res) => {
+    req.params.categoria = 'Tematicas';
+    velasController.getVelasByCategoria(req, res);
+  });
+  
+  router.get('/velasDecorativas', (req, res) => {
+    req.params.categoria = 'Decorativas';
+    velasController.getVelasByCategoria(req, res);
+  });
 module.exports = router;

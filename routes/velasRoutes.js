0bccc11b12cf290bf7f1cb841fad423539f69976
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-12-13 11:45:16 +0100

    modified:   routes/velasRoutes.js

--- routes/velasRoutes.js
+++ routes/velasRoutes.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const velasController = require('../controllers/velasController');
 
 // Rutas para cada categoría
-router.get('/velas/velasAromaticas', (req, res) => {
+router.get('/velasAromaticas', (req, res) => {
   req.params.categoria = 'Aromaticas';
   velasController.getVelasByCategoria(req, res);
 });
   
-  router.get('/velas/velasTematicas', (req, res) => {
+  router.get('/velasTematicas', (req, res) => {
     req.params.categoria = 'Tematicas';
     velasController.getVelasByCategoria(req, res);
   });
   
-  router.get('/velas/velasDecorativas', (req, res) => {
+  router.get('/velasDecorativas', (req, res) => {
     req.params.categoria = 'Decorativas';
     velasController.getVelasByCategoria(req, res);
   });

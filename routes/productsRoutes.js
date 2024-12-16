7b1403053fbbd148dcec6a576646ce36d76770c6
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-12-16 14:29:39 +0100

    modified:   routes/productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getAllProducts } = require('../controllers/productsController');
 
-router.get('/', getAllProducts);
+router.get('/productos', getAllProducts);
 
 module.exports = router;

e575b3adcb58a5b6258c3f4c962727c7f7779512
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-12-16 14:27:29 +0100

    modified:   routes/userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Ruta para registrar usuarios
 router.post('/register', userController.registerUser);
 // Ruta para login
-router.post("/api/login", userController.loginUser);
+router.post("/login", userController.loginUser);
 // Ruta para logout
 router.get('/logout', userController.logoutUser);
 

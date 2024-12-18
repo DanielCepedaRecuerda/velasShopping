2511885deb87681875309c676194d2737f0b14b2
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-12-18 11:54:57 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes"); // Ruta de los controladores
 const cartRoutes = require('./routes/cartRoutes');
 const velasRoutes = require('./routes/velasRoutes');
 const contactRoutes = require('./routes/contactRoutes');
+const checkoutRoutes = require('./routes/checkoutRoutes'); // Asegúrate de que la ruta sea correcta
 const productsRoutes = require('./routes/productsRoutes');
 const { logoutUser } = require("./controllers/userController");
 const path = require("path");
@@ -70,6 +71,7 @@ app.use('/api/contact', contactRoutes);
 app.use('/velas', velasRoutes);
 app.use('/cart', cartRoutes);
 app.use('/productos', productsRoutes);
+app.use('/', checkoutRoutes);
 
 // Iniciar servidor
 app.listen(3000, () => {

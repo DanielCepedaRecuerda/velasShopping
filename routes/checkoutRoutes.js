eb53ce880c28d20602c9069d4387f3beb40a7dd0
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-12-19 09:17:25 +0100

    modified:   routes/checkoutRoutes.js

--- routes/checkoutRoutes.js
+++ routes/checkoutRoutes.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const checkoutController = require('../controllers/checkoutController');
 
 // Ruta para mostrar la vista de checkout
-router.get('/', checkoutController.showCheckout);
-
+app.get('/checkout', isAuthenticated, checkoutController.showCheckout);
 // Ruta para procesar el formulario de checkout
 router.post('/process', checkoutController.processCheckout);
 

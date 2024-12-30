fd9ee815bf5198c63e848a04367010977e0c9071
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2024-12-30 18:51:49 +0100

    modified:   controllers/contactController.js

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,6 +2,7 @@ const nodemailer = require('nodemailer');
 // Controlador para manejar el formulario de contacto
 exports.handleContactForm = async (req, res) => {
     const { name, email, message } = req.body;
+    console.log(name, email, message);
     
     try {
         // Aquí va la lógica de enviar un correo electrónico

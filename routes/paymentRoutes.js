42d077f4039832678aad02d060aa13dbdfeefa72
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2025-01-09 19:27:37 +0100

    deleted:    routes/paymentRoutes.js

--- routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const paymentController = require("../controllers/paymentController");
-
-// Ruta para la confirmación de pagos
-router.get("/confirmation", paymentController.confirmPayment);
-
-module.exports = router;

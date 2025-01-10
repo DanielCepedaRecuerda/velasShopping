9f881f3d7d73a961e2bd6b219b98874327d58c12
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2025-01-10 13:31:32 +0100

    modified:   controllers/paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -14,16 +14,14 @@ const confirmPayment = async (req, res) => {
     // Obtener los artículos del carrito desde la base de datos
     const cartItems = await cartModel.getCart(cartId);
 
+    // Vaciar el carrito después del pago
+    await cartModel.clearCart(cartId);
+
     // Renderizar la vista de confirmación con los artículos del carrito
     res.render("confirmation", {
       message: "Tu pago se procesó exitosamente.",
       cartItems: cartItems,
     });
-
-    // Opcional: Vaciar el carrito después del pago
-    await cartModel.clearCart(cartId);
-
-    res.redirect("/confirmation");
   } catch (error) {
     console.error("Error al confirmar el pago:", error);
     res.status(500).send("Ocurrió un error al procesar tu pago.");

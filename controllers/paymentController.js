8013729ff06272a441f2e3e0a7f582562f21edbb
DanielCepedaRecuerda <danielcepedarecuerda@gmail.com>
2025-01-10 17:32:40 +0100

    modified:   controllers/paymentController.js 	modified:   views/confirmation.ejs

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -8,7 +8,7 @@ const paymentController = async (req, res) => {
     console.log("Form: " + numeroTarjeta, nombreTitular, fechaExpiracion, cvv);
     
     // Recuperar la cookie "cart" (cartId o los datos del carrito)
-    const cart = req.cookies.cart;
+    const cart = req.cookies.cart ? JSON.parse(req.cookies.cart) : []; // Recuperar los datos del carrito desde la cookie
     console.log("Cookie: " + cart);
 
     // Si no se encuentra un carrito o no se envían los datos del formulario, retornar un error
@@ -20,6 +20,7 @@ const paymentController = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "Pago procesado correctamente. Redirigiendo a la confirmación.",
+      cartItems: cart,
     });
 
     // Aquí podrías agregar la lógica de inserción a base de datos o procesamiento del pago
